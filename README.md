a4e717eff3cddc741f08a94ccedfebd0785da539
PoliukhovychAnna <116736506+PoliukhovychAnna@users.noreply.github.com>
2023-09-13 02:12:30 +0300

    done

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # goit-node.js-hw-01
+
+https://monosnap.com/list/6500edd95c7a789d8ff8bed7
--- contacts.js
+++ contacts.js
@@ -7,6 +7,7 @@ const contactsPath = path.join(__dirname, "db", "contacts.json");
 // Returns an array of contacts.
 async function listContacts() {
   const contacts = JSON.parse(await fs.readFile(contactsPath));
+  console.table(contacts);
   return contacts;
 }
 
@@ -15,6 +16,7 @@ async function getContactById(contactId) {
   const contacts = await listContacts();
   const contactToFind =
     contacts.find((contact) => contact.id === contactId) || null;
+  console.log(contactToFind);
   return contactToFind;
 }
 
@@ -23,29 +25,36 @@ async function removeContact(contactId) {
   const contacts = await listContacts();
   const index = contacts.findIndex((contact) => contact.id === contactId);
   if (index === -1) {
+    console.log(null);
     return null;
   }
 
   const [deletedContact] = contacts.splice(index, 1);
 
   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+  console.log(deletedContact);
   return deletedContact;
 }
 
 //Returns object of added contact
 async function addContact(name, email, phone) {
   const contacts = await listContacts();
-  const newContact = {
-    id: nanoid(),
-    name,
-    email,
-    phone,
-  };
-
-  contacts.push(newContact);
-
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-  return newContact;
+  const doesExist = contacts.find(
+    (contact) => contact.email === email || contact.phone === phone
+  );
+  if (!doesExist) {
+    const newContact = {
+      id: nanoid(),
+      name: name,
+      email: email,
+      phone: phone,
+    };
+    contacts.push(newContact);
+    console.log(`NEWCONTACT >>>>>>> ${JSON.stringify(newContact)}`);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return newContact;
+  }
 }
 
 module.exports = {
